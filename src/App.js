6fb8539e8f3eb11088320094faff31677b292dd6
Gustaocta <100249655+Gustaocta@users.noreply.github.com>
2022-04-29 10:04:09 -0300

    Add pasta do projeto React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cabecalho">cabecalho</div>
+        <div className="menu">menu</div>
+          <div className="conteudo">conteudo</div>
+           <div className="galeria_fotos">galeria</div>
+             <footer className="rodape">rodape
+             
+                <div className="insta_icon">logo_insta</div>
+                <div className="face_icon">logo_face</div>
+                <div className="twitter_icon">logo_twitter</div>
+                <div className="whats_icon">logo_whats</div>
+                <div className="pinter_icon">logo_pinter</div>
+
+             </footer>
+
+    </div>
+  );
+}
+
+export default App;
